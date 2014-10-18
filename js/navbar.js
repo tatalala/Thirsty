307d870461bb876095696856a8755a37ff0af530
ScottDeJesus <sdjesus89@gmail.com>
2014-10-18 14:38:38 -0700

    Navbar title displays and color changes

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+$(function(){
+			//if user is logged in {
+
+			//open navbar_loggedIn.html
+
+
+			//else
+          	$("#navbar").load("navbar.html");
+        	//}  
+    
+
+    	//if media.html OR about.html
+
+    	$("#socialBtns").load("socialBtns.html");
+});
