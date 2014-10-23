c9702080291958c9617564632e9689255d4d3fae
Diego Tobar <dl_tobar@yahoo.com>
2014-10-23 15:59:42 -0700

    Removed replaced the index.html page with the inspire.html; bug fixes;

--- js/script.js
+++ js/script.js
@@ -1,69 +1,69 @@
 /* Login script */
 Parse.initialize("YgscJuPF5n0Tab0kShVye4KdCKQZx8E3yzdC8804", "o5kLLsJQTT4Xc5yR4FlQHngF31txDtfXDnTREq1C");
 
-function logIn(){
-     var username = document.getElementById('1').value;
-     var password = document.getElementById('2').value;
-     Parse.User.logIn(username, password,
-    {
-        success: function(user) 
-        {
-           location.href='user_profile.html';
-        },
-        error: function(user, error) 
-        {
-            alert("Incorrect password. Please try again.");
-        }
-    });
-};
-/* End of login script */
-
-/* Signup script */
-function signUp(){
-    var user = new Parse.User();
-    user.set("username", document.getElementById('a').value);
-    user.set("password", document.getElementById('b').value);
-    user.set("email", document.getElementById('c').value);
-
-    user.signUp(null,
-    {
-        success: function(user) 
-        {
-            location.href = 'inspire.html';
-        },
-        error: function(user, error) 
-        {
-            alert("Error: " + error.code + " " + error.message);
-        }
-    });
-};
-
-/* End of signup script */
-function loggedIn(){
-    var currentUser = Parse.User.current();
-    if(currentUser){
-
-    }
-    else{
-
-    }
-}
-
-$(document).ready(function(){
-    // $("#updateInfo").submit(function(e){
-    //       e.preventDefault();
-    //       updateProf();
-    // });
-    // retrieveUserInfo(); 
-    updateProf();  
-    retrieveUserInfo();
-  })
+//function logIn(){
+//     var username = document.getElementById('1').value;
+//     var password = document.getElementById('2').value;
+//     Parse.User.logIn(username, password,
+//    {
+//        success: function(user) 
+//        {
+//           location.href='user_profile.html';
+//        },
+//        error: function(user, error) 
+//        {
+//            alert("Incorrect password. Please try again.");
+//        }
+//    });
+//};
+///* End of login script */
+//
+///* Signup script */
+//function signUp(){
+//    var user = new Parse.User();
+//    user.set("username", document.getElementById('a').value);
+//    user.set("password", document.getElementById('b').value);
+//    user.set("email", document.getElementById('c').value);
+//
+//    user.signUp(null,
+//    {
+//        success: function(user) 
+//        {
+//            location.href = 'inspire.html';
+//        },
+//        error: function(user, error) 
+//        {
+//            alert("Error: " + error.code + " " + error.message);
+//        }
+//    });
+//};
+//
+///* End of signup script */
+//function loggedIn(){
+//    var currentUser = Parse.User.current();
+//    if(currentUser){
+//
+//    }
+//    else{
+//
+//    }
+//}
+//
+//$(document).ready(function(){
+//    // $("#updateInfo").submit(function(e){
+//    //       e.preventDefault();
+//    //       updateProf();
+//    // });
+//    // retrieveUserInfo(); 
+//    updateProf();  
+//    retrieveUserInfo();
+//  })
 
   function updateProf(){
 
-    var User = Parse.Object.extend("User");
+    //var User = Parse.Object.extend("User");
     var currentUser = Parse.User.current();
-    var query= new Parse.Query(User);
+    var query= new Parse.Query(currentUser);
     var currentUserID = currentUser.id;
 
     var email = $("#email").val();
