70393a4bff95a5c80f43f18da432bd8c036badb9
Duy Mai <duyqmai@gmail.com>
2014-10-23 23:06:47 -0700

    Add confirm password to signup page. Second attempt at calling log in , log out, and sign up functions from script.js

--- js/script.js
+++ js/script.js
@@ -2,39 +2,42 @@
 Parse.initialize("YgscJuPF5n0Tab0kShVye4KdCKQZx8E3yzdC8804", "o5kLLsJQTT4Xc5yR4FlQHngF31txDtfXDnTREq1C");
 
 function logIn(){
-     var username = document.getElementById('1').value;
-     var password = document.getElementById('2').value;
-     Parse.User.logIn(username, password,
-    {
+     Parse.User.logIn(document.getElementById('1').value, document.getElementById('2').value,
+     {
         success: function(user) 
         {
-           location.href='user_profile.html';
-        },
-        error: function(user, error) 
-        {
-            alert("Incorrect password. Please try again.");
-        }
-    });
-};
-/* End of login script */
-
-/* Signup script */
-function signUp(){
-    var user = new Parse.User();
-    user.set("username", document.getElementById('a').value);
-    user.set("password", document.getElementById('b').value);
-    user.set("email", document.getElementById('c').value);
-
-    user.signUp(null,
-    {
-        success: function(user) 
-        {
-            location.href = 'inspire.html';
+            location.href = "user_profile.html";
         },
         error: function(user, error) 
         {
             alert("Error: " + error.code + " " + error.message);
         }
-    });
+     });
 };
+///* Signup script */
+function signUp(){
+        var user = new Parse.User();
+        var confirmPass = document.getElementById('confirmpass').value;
+        var userPass =  document.getElementById('userpass').value;
+        if(confirmPass == userPass){
+            user.set("username", document.getElementById('a').value);
+            user.set("password", userPass);
+            user.set("email", document.getElementById('c').value);
+            user.signUp(null,{
+              success: function(user){
+                location.href = 'user_profile.html';
+              },
+              error: function(user, error){
+                alert("Error: " + error.code + " " + error.message);
+              }
+            });
+        }
+        else{
+            alert("Error: Password does not match");
+        }
+      };
 
+function loggedout(){
+				Parse.User.logOut();
+				location.href = "inspire.html";
+};
